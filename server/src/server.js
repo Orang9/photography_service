1a812237aa883bd24dad8f8eeed729e573b71273
Orang9 <egbert.fewi@gmail.com>
2025-12-29 15:30:55 +0700

    clear console.log from server

--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,6 @@
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
-
-console.log({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+dotenv.config();
 
 import app from "./app.js";
 
